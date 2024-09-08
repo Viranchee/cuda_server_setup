80b167d71ecdb49d498128be23885dbee6e5d60a
Viranchee Lotia <hey@viranchee.com>
2024-09-08 15:33:11 -0400

    Code to setup fish shell

---
+++ add_fish.sh
@@ -0,0 +1,8 @@
+# If fish is already installed, skip the installation
+if ! command -v fish &> /dev/null; then
+    sudo apt-add-repository ppa:fish-shell/release-3 -y
+    sudo apt update
+    sudo apt install fish -y
+fi
+
+exec fish
